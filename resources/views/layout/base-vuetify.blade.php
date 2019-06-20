e8f60531f48c86e4fc12b57f3c34005586e321d5
Lingyong Sun <lysz210@gmail.com>
2019-06-20 22:31:49 +0200

    update

--- resources/views/layout/base-vuetify.blade.php
+++ resources/views/layout/base-vuetify.blade.php
@@ -1,6 +1,7 @@
 @extends('layout.base')
 
 @section('head')
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
