24c7e67b44215d7f954eaeae2826045a1f966fe3
Lingyong Sun <lysz210@gmail.com>
2019-06-29 01:07:42 +0200

    debug

--- config/snappy.php
+++ config/snappy.php
@@ -7,17 +7,17 @@ return array(
         'enabled' => true,
         'binary'  => env('WKHTMLTOPDF_BIN_DIR') . '/wkhtmltopdf',
         'timeout' => false,
-        'options' => array(),
+        'options' => array(
+            'dpi' => 144,
+            'print-media-type' => true,
+        ),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
         'binary'  => env('WKHTMLTOPDF_BIN_DIR') . '/wkhtmltoimage',
         'timeout' => false,
-        'options' => array(
-            'dpi' => 144,
-            'print-media-type' => true,
-        ),
+        'options' => array(),
         'env'     => array(),
     ),
 
