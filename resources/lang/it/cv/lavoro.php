98fab187efc6bf95eb48391dfc1a363969699067
Lingyong Sun <lysz210@gmail.com>
2021-06-26 23:01:29 +0200

    i18n lavoro

---
+++ resources/lang/it/cv/lavoro.php
@@ -0,0 +1,62 @@
+<?php return [
+    "title" => "Esperienze professionali",
+    "esperienze" => [
+        "ti" => [
+            "periodo" => "Occupazione attuale da Gennaio 2019",
+            "ruolo" => 'Consulente informatico cliente principale <a href="https://www.reply.com/it/" alt="link al sito Reply" target="blank">Reply</a> di Silea',
+            "azienda" => '<a href="https://www.telematicainformatica.it/" alt="link al sito Telematica Informatica Srl" target="blank">Telematica Informatica Srl</a>,
+            Corso Rosselli 240, Torino 10141',
+            "attivita" => [
+                "Sviluppo front-end in Java",
+                "Tecnologie principali: Spring, Primefaces, sass, JBoss",
+                "Versioning con GitLab",
+            ],
+            "settore" => "Informatica",
+        ],
+        "tech" => [
+            "periodo" => "Dal Agosto 2016 al Dicembre 2018",
+            "ruolo" => 'Web developer',
+            "azienda" => '<a href="http://www.studioitaliatech.com/" alt="link al sito Studio Italia Tech Srl" target="blank">Studio Italia Tech s.r.l.</a>,
+            Via Pialoi 30 - 30020 Marcon - Venezia (Italia)',
+            "attivita" => [
+                "Sviluppo back-end, principalmente in php",
+                "Sviluppo front-end",
+                "Sviluppo applicazioni con Electron",
+                "Automazione di alcuni processi ripetitivi emulabili attraverso script con linguaggi di programmazione"
+            ],
+            "settore" => "Informatica",
+        ],
+        "flunch" => [
+            "periodo" => "Dal 2004 al 21 Giugno 2013",
+            "ruolo" => 'Pizzaiolo',
+            "azienda" => '<a href="http://www.flunch.it/" alt="link al sito ufficiale flunch italia" target="blank">Flunch Italia s.r.l.</a>, via Don Tosatto 1, Mestre(Venezia)',
+            "attivita" => [
+                "Produzione e vendita di pizze al trancio",
+                "Mantenimento dell'ordine magazzino alimentare"
+            ],
+            "settore" => "Ristorazione self-service",
+        ],
+        "mc" => [
+            "periodo" => "Dal 2002 al 2004",
+            "ruolo" => 'Adetto generico',
+            "azienda" => '<a href="http://www.caribesrl.com/index.html" alt="link al sito ufficiale dell\'azienda Caribe srl" target="blank">Caribe s.r.l.</a>, McDonald\'s in franchising, calle Larga San Marco Venezia',
+            "attivita" => [
+                "Banconiere",
+                "Produzione panini",
+                "inizio formazione nel ruolo manageriale"
+            ],
+            "settore" => "Ristorazione fast-food",
+        ],
+        "famiglia" => [
+            "periodo" => "Dal 1996 al 2002",
+            "ruolo" => 'Lavoro in azienda familiare',
+            "azienda" => 'Friendshop of the world (Azienda familiare)',
+            "attivita" => [
+                "Vendita al dettaglio di accessori di abbigliamento (principalmente borse in pelle)",
+                "Produzione di borse in pelle",
+                "Ricamo su magliette"
+            ],
+            "settore" => "Vendita al dettaglio di prodotti non alimentari, lavorazione artigianale di pellame e abbigliamento.",
+        ]
+    ]
+];
