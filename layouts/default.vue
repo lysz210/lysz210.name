4da486586f18476ba820eee29a3a89201a80c986
Lingyong Sun <lingyong.sun@gmail.com>
2023-01-14 18:54:10 +0100

    add socials

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,22 +3,22 @@
     <v-app-bar app color="blue-grey" dark>
       <v-toolbar-title>Lysz210</v-toolbar-title>
       <v-spacer></v-spacer>
-      <transition-group name="fade" @enter="enter">
-        <v-btn
-          v-for="social in socials"
-          :key="social.name"
-          :href="social.url"
-          icon
-          small
-          target="_blank"
-          style="background-color: white; margin: .3rem"
-        >
-          <v-icon
-            v-text="social.icon"
-            :style="{ color: social.color || 'black' }"
-          ></v-icon>
-        </v-btn>
-      </transition-group>
+      <v-btn
+        v-for="social in socials"
+        :key="social.name"
+        :href="social.url"
+        icon
+        small
+        target="_blank"
+        style="background-color: white; margin: .3rem"
+      >
+        <v-icon
+          :style="{ color: social.color || 'black' }"
+        >{{ social.icon }}</v-icon>
+      </v-btn>
+      <!-- <transition-group name="fade" @enter="enter">
+        
+      </transition-group> -->
     </v-app-bar>
     <v-main><v-container><slot></slot></v-container></v-main>
     <v-footer app color="blue-grey">
@@ -36,3 +36,29 @@ html {
   overflow-y: auto;
 }
 </style>
+<script>
+import { ajax } from 'rxjs/ajax'
+import { map } from 'rxjs/operators'
+
+export default {
+  data: () => {
+    return {
+      socials: [],
+    }
+  },
+  mounted() {
+    ajax({
+      url: "http://cv.lysz210.name.s3-website.eu-south-1.amazonaws.com/me/social-accounts.json",
+      method: 'GET',
+      crossDomain: true,
+      withCredentials: false
+    })
+    .pipe(map(response => response?.response))
+    .subscribe({
+      next: (socialsData) => {
+        this.socials.push(...socialsData)
+      }
+    })
+  }
+}
+</script>
