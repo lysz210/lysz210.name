680889be31a0a286d5bc63f55c15deb53b3bf92c
Lingyong Sun <lingyong.sun@gmail.com>
2023-01-22 19:26:20 +0100

    provissioning website to traefik

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -3,6 +3,10 @@ import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 import * as glob from 'glob';
 import * as mime from 'mime';
+import { remote } from '@pulumi/command'
+
+const configs = new pulumi.Config();
+
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("lysz210.name", {
     bucket: 'lysz210.name',
@@ -26,6 +30,16 @@ glob.sync('**/*', {
         acl: 'public-read',
         contentType: mimeType || undefined
     })
+})
+
+const cvTraefik = new remote.CopyFile('traefik', {
+    connection: {
+        host: 'lysz210.name',
+        user: 'ubuntu',
+        privateKey: configs.requireSecret('awsMiPem')
+    },
+    localPath: './website-traefik.yaml',
+    remotePath: 'swarm-controller/configs/traefik/website.yaml'
 })
 
 // Export the name of the bucket
