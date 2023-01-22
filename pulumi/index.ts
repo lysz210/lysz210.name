49cbaa03df1ec0d9745a7e9161d17d0d66e49387
Lingyong Sun <lingyong.sun@gmail.com>
2023-01-22 21:37:25 +0100

    import traefik stack

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -4,8 +4,13 @@ import * as awsx from "@pulumi/awsx";
 import * as glob from 'glob';
 import * as mime from 'mime';
 import { remote } from '@pulumi/command'
+import { Output } from "@pulumi/pulumi";
 
 const configs = new pulumi.Config();
+const org = pulumi.getOrganization();
+const stack = pulumi.getStack();
+
+const traefikRef = new pulumi.StackReference(`${org}/traefik.lysz210.name/${stack}`)
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("lysz210.name", {
@@ -32,14 +37,21 @@ glob.sync('**/*', {
     })
 })
 
+interface SshConnection {
+    user: Output<string>;
+    host: Output<string>;
+}
+
+const connection = traefikRef.requireOutput('connection') as Output<SshConnection>
+
 const cvTraefik = new remote.CopyFile('traefik', {
     connection: {
-        host: 'lysz210.name',
-        user: 'ubuntu',
+        user: connection.user,
+        host: connection.host,
         privateKey: configs.requireSecret('awsMiPem')
     },
     localPath: './website-traefik.yaml',
-    remotePath: 'swarm-controller/configs/traefik/website.yaml'
+    remotePath: pulumi.interpolate `${traefikRef.requireOutput('dynamicConfigsPath')}/website.yaml`
 })
 
 // Export the name of the bucket
