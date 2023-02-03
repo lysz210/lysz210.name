9b209d36ba8814f0407f095e8c8c0adc06e31151
Lingyong Sun <lingyong.sun@gmail.com>
2023-02-03 21:08:43 +0100

    properly trigger copyfile
    temporary solution adding hash of file content in pulumi resource name

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -5,6 +5,8 @@ import * as glob from 'glob';
 import * as mime from 'mime';
 import { remote } from '@pulumi/command'
 import { Output } from "@pulumi/pulumi";
+import { readFileSync } from "fs";
+import * as md5 from "md5";
 
 const configs = new pulumi.Config();
 const org = pulumi.getOrganization();
@@ -13,11 +15,30 @@ const stack = pulumi.getStack();
 const traefikRef = new pulumi.StackReference(`${org}/traefik.lysz210.name/${stack}`)
 
 // Create an AWS resource (S3 Bucket)
+const PublicRead = aws.s3.CannedAcl.PublicRead
+const logsBucket = new aws.s3.Bucket("logs.lysz210.name")
 const bucket = new aws.s3.Bucket("lysz210.name", {
     bucket: 'lysz210.name',
+    acl: PublicRead,
     website: {
-        indexDocument: "index.html"
+        indexDocument: "index.html",
+        routingRules: [
+            {
+                Condition: {
+                    HttpErrorCodeReturnedEquals: '404'
+                },
+                Redirect: {
+                    ReplaceKeyWith: '404.html'
+                }
+            }
+        ]
     },
+    loggings: [
+        {
+            targetBucket: logsBucket.id,
+            targetPrefix: 'log/'
+        }
+    ]
 });
 
 const dist = '../.output/public/';
@@ -32,7 +53,7 @@ glob.sync('**/*', {
     new aws.s3.BucketObject(file, {
         bucket: bucket.id,
         source: new pulumi.asset.FileAsset(filePath),
-        acl: 'public-read',
+        acl: PublicRead,
         contentType: mimeType || undefined
     })
 })
@@ -41,16 +62,20 @@ interface SshConnection {
     user: Output<string>;
     host: Output<string>;
 }
-
+// add suffix in name to ensure trigger changes
+// on file channge.
+// This is a temporary solution, need a proper
+// change detection on contente in resource log
+const treafikFilePath = './website-traefik.yaml';
+const hash = md5(readFileSync(treafikFilePath));
 const connection = traefikRef.requireOutput('connection') as Output<SshConnection>
-
-const cvTraefik = new remote.CopyFile('traefik', {
+const cvTraefik = new remote.CopyFile(`traefik-${hash}`, {
     connection: {
         user: connection.user,
         host: connection.host,
         privateKey: configs.requireSecret('awsMiPem')
     },
-    localPath: './website-traefik.yaml',
+    localPath: treafikFilePath,
     remotePath: pulumi.interpolate `${traefikRef.requireOutput('dynamicConfigsPath')}/website.yaml`
 })
 
