daa6231123b4dd202af73effc0c27696bcfcb4b5
Lingyong Sun <lingyong.sun@gmail.com>
2024-12-30 15:17:54 +0100

    update modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false,
-    app: {
-        head: {
-            title: 'Lysz210'
-        }
-    },
-    runtimeConfig: {
-        public: {
-            cvBasePath: 'https://cv.lysz210.name',
-            cvPdf: '/CV_lingyong_sun.pdf',
-            meSocialAccounts: '/me/social-accounts'
-        }
-    },
-    vite: {
-        ssr: {
-            noExternal: ['vuetify']
-        }
-    },
-    generate: {
-        routes: [ '/en', '/it' ]
-    }
+  ssr: false,
+
+  app: {
+      head: {
+          title: 'Lysz210'
+      }
+  },
+
+  runtimeConfig: {
+      public: {
+          cvBasePath: 'https://cv.lysz210.name',
+          cvPdf: '/CV_lingyong_sun.pdf',
+          meSocialAccounts: '/me/social-accounts'
+      }
+  },
+
+  vite: {
+      ssr: {
+          noExternal: ['vuetify']
+      }
+  },
+
+  generate: {
+      routes: [ '/en', '/it' ]
+  },
+
+  compatibilityDate: '2024-12-30'
 })
